cc4ce7823fe9e29abee309c968e7355e03a8ec53
Madzi Hove <madzihove@gmail.com>
2023-03-16 17:35:57 +0200

    add button event listeners

--- js/script.js
+++ js/script.js
@@ -1,4 +1,41 @@
-let myLibrary = [];
+// buttone containers
+const buttonContainer = document.querySelector(".button__container");
+const secondaryBtn = document.querySelector(".secondary-btn");
+// buttons
+const newBtn = document.querySelector("#new-book-btn");
+const addBtn = document.querySelector("#add-book-btn");
+const cancelBtn = document.querySelector("#cancel-btn");
+// form
+const form = document.querySelector("form");
+const fieldset = document.querySelector("fieldset");
+// inputs
+const title = document.querySelector("#title");
+const author = document.querySelector("#author");
+const pages = document.querySelector("#pages");
+const read = document.querySelector("#read");
+// table
+const table = document.querySelector(".table");
+
+let myLibrary = [
+	{
+		title: "Excession",
+		author: "Ian M. Banks",
+		pages: 451,
+		read: "Yes",
+	},
+	{
+		title: "The Colour of Magic",
+		author: "Terry Pratchett",
+		pages: 240,
+		read: "Yes",
+	},
+	{
+		title: "Gardens of the Moon",
+		author: "Steven Erikson",
+		pages: 491,
+		read: "Yes",
+	},
+];
 
 function Books(title, author, pages, read) {
 	this.title = title;
@@ -12,10 +49,30 @@ Books.prototype.report = function () {
 };
 
 function addBookToLibrary(title, author, pages, read) {
-	title = new Books(title, author, pages, read);
-	myLibrary.push(title);
+	myLibrary.push(new Books(title, author, pages, read));
 }
-addBookToLibrary("The hobbit", "Tolkien", 295, "read");
-addBookToLibrary("Look to windward", "Tolkien", 295, "read");
 
 console.log(myLibrary);
+
+function buttonOperations(target) {
+	if (target.id === newBtn.id) {
+		form.classList.remove("closed");
+		form.classList.add("expand");
+		fieldset.removeAttribute("hidden");
+		secondaryBtn.removeAttribute("hidden");
+		newBtn.setAttribute("hidden", "hidden");
+	}
+	if (target.id === cancelBtn.id) {
+		form.classList.remove("expand");
+		form.classList.add("closed");
+		fieldset.setAttribute("hidden", "hidden");
+		secondaryBtn.setAttribute("hidden", "hidden");
+		newBtn.removeAttribute("hidden");
+	}
+	if (target.id === addBtn.id) {
+		const bookRead = read.checked ? "Yes" : "No";
+		addBookToLibrary(title.value, author.value, pages.value, bookRead);
+	}
+}
+
+buttonContainer.addEventListener("click", (e) => buttonOperations(e.target));
