d4defcad6e7e022e5f50be62b6b1754f52795b8d
Madzi Hove <madzihove@gmail.com>
2023-05-01 17:50:11 +0200

    change from constructor functions to classes

--- js/script.js
+++ js/script.js
@@ -18,108 +18,67 @@ const readInput = document.querySelector("#read");
 // table
 const table = document.querySelector("table");
 const tableBody = document.querySelector("tbody");
-let myLibrary = [];
-let exampleData = [
-	{
-		title: "Excession",
-		author: "Ian M. Banks",
-		pages: 451,
-		read: { checked: true },
-	},
-	{
-		title: "The Colour of Magic",
-		author: "Terry Pratchett",
-		pages: 240,
-		read: { checked: true },
-	},
-	{
-		title: "Gardens of the Moon",
-		author: "Steven Erikson",
-		pages: 491,
-		read: { checked: true },
-	},
-];
 
-function exampleBooks() {
-	exampleData.forEach((book) => {
-		addBookToLibrary(book.title, book.author, book.pages, book.read);
-	});
-}
-
-// constructor
-function Book(title, author, pages, read) {
-	const bookRead = read.checked ? "Yes" : "No";
-	this.title = title;
-	this.author = author;
-	this.pages = pages;
-	this.read = bookRead;
-}
-
-Book.prototype.changeReadStatus = function () {
-	this.read = this.read === "Yes" ? "No" : "Yes";
-	displayTable();
-};
-
-// app interface
-function showForm() {
-	form.classList.remove("closed");
-	form.classList.add("expand");
-	fieldset.removeAttribute("hidden");
-	secondaryBtn.removeAttribute("hidden");
-	newBtn.setAttribute("hidden", "hidden");
-}
-
-function hideForm() {
-	form.classList.remove("expand");
-	form.classList.add("closed");
-	fieldset.setAttribute("hidden", "hidden");
-	secondaryBtn.setAttribute("hidden", "hidden");
-	newBtn.removeAttribute("hidden");
-}
-
-function buttonOperations(target) {
-	if (target.id === newBtn.id) {
-		showForm();
+class Book {
+	constructor(title, author, pages, read) {
+		this.bookRead = read.checked ? "Yes" : "No";
+		this.title = title;
+		this.author = author;
+		this.pages = pages;
+		this.read = this.bookRead;
 	}
-	if (target.id === cancelBtn.id) {
-		hideForm();
+
+	changeReadStatus() {
+		this.read = this.read === "Yes" ? "No" : "Yes";
+		app.displayTable();
 	}
-	if (target.id === addBtn.id) {
-		addBookToLibrary(
-			titleInput.value,
-			authorInput.value,
-			pagesInput.value,
-			readInput.value
-		);
+}
+
+class Library {
+	myLibrary = [];
+	exampleData = [
+		{
+			title: "Excession",
+			author: "Ian M. Banks",
+			pages: 451,
+			read: { checked: true },
+		},
+		{
+			title: "The Colour of Magic",
+			author: "Terry Pratchett",
+			pages: 240,
+			read: { checked: true },
+		},
+		{
+			title: "Gardens of the Moon",
+			author: "Steven Erikson",
+			pages: 491,
+			read: { checked: true },
+		},
+	];
+	constructor() {
+		this.exampleBooks();
+		this.displayTable();
+		main.addEventListener("click", e => this.buttonOperations(e.target));
+		form.addEventListener("submit", e => this.addBookToTable(e));
 	}
-	if (target.classList.contains("delete")) {
-		myLibrary.splice(+target.dataset.indexNumber, 1);
-		tableBody.querySelectorAll("tr").forEach((child) => {
+	exampleBooks() {
+		this.exampleData.forEach(book => {
+			this.addBookToLibrary(book.title, book.author, book.pages, book.read);
+		});
+	}
+
+	// library operation
+	addBookToLibrary(title, author, pages, read) {
+		this.myLibrary.push(new Book(title, author, pages, read));
+	}
+
+	displayTable() {
+		tableBody.querySelectorAll("tr").forEach(child => {
 			child.remove();
 		});
-		displayTable();
-	}
-
-	if (target.classList.contains("toggle-btn")) {
-		myLibrary[target.dataset.indexNumber].changeReadStatus();
-	}
-}
-// library operation
-function addBookToLibrary(title, author, pages, read) {
-	myLibrary.push(new Book(title, author, pages, read));
-}
-
-function addBookToTable(event) {
-	event.preventDefault();
-	displayTable();
-}
-
-function displayTable() {
-	tableBody.querySelectorAll("tr").forEach((child) => {
-		child.remove();
-	});
-	myLibrary.forEach((book, i) => {
-		const html = `<tr class="row">
+		this.myLibrary.forEach((book, i) => {
+			const html = `<tr class="row">
 						<td class="number row-data" scope="row">${i + 1}</td>
 						<td class="title row-data" scope="row">${book.title}</td>
 						<td class="author row-data" scope="row">${book.author}</td>
@@ -131,14 +90,59 @@ function displayTable() {
 						</span></button></td>
 					</tr>`;
 
-		tableBody.insertAdjacentHTML("beforeend", html);
-	});
+			tableBody.insertAdjacentHTML("beforeend", html);
+		});
+	}
+
+	buttonOperations = target => {
+		if (target.id === newBtn.id) {
+			this.showForm();
+		}
+		if (target.id === cancelBtn.id) {
+			this.hideForm();
+		}
+		if (target.id === addBtn.id) {
+			this.addBookToLibrary(
+				titleInput.value,
+				authorInput.value,
+				pagesInput.value,
+				readInput.value
+			);
+		}
+		if (target.classList.contains("delete")) {
+			this.myLibrary.splice(+target.dataset.indexNumber, 1);
+			tableBody.querySelectorAll("tr").forEach(child => {
+				child.remove();
+			});
+			this.displayTable();
+		}
+
+		if (target.classList.contains("toggle-btn")) {
+			this.myLibrary[target.dataset.indexNumber].changeReadStatus();
+		}
+	};
+
+	// app interface
+	showForm() {
+		form.classList.remove("closed");
+		form.classList.add("expand");
+		fieldset.removeAttribute("hidden");
+		secondaryBtn.removeAttribute("hidden");
+		newBtn.setAttribute("hidden", "hidden");
+	}
+
+	hideForm() {
+		form.classList.remove("expand");
+		form.classList.add("closed");
+		fieldset.setAttribute("hidden", "hidden");
+		secondaryBtn.setAttribute("hidden", "hidden");
+		newBtn.removeAttribute("hidden");
+	}
+
+	addBookToTable = event => {
+		event.preventDefault();
+		this.displayTable();
+	};
 }
 
-window.addEventListener("load", () => {
-	exampleBooks();
-	displayTable();
-	main.addEventListener("click", (e) => buttonOperations(e.target));
-
-	form.addEventListener("submit", (e) => addBookToTable(e));
-});
+const app = new Library();
